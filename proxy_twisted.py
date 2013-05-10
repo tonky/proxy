05766979473c8d0f18bb8b598b24ed0bdadbcf7d
Igor Tonky <tonky@zojax.com>
2013-05-10 17:20:48 +0300

    changed django to twisted

--- proxy_twisted.py
+++ proxy_twisted.py
@@ -1,14 +1,83 @@
+import re
+from urlparse import urlsplit, urlunsplit
+import requests
 from twisted.internet import reactor
-from twisted.web.server import Site
 from twisted.web.resource import Resource
-import time
+from twisted.web.server import Site
+from tunneler import replace_links
 
-class ClockPage(Resource):
+
+cookies = {}
+
+
+class Dispatch(Resource):
+    def getChild(self, name, request):
+        scheme = "http"
+
+        if request.isSecure():
+            scheme = "https"
+
+        tunnel_path_match = re.search("/tunnel/(\S+)", request.uri)
+
+        if tunnel_path_match:
+            client_host = "%s://%s:%d/tunnel" % (scheme, request.getHost().host, request.getHost().port)
+            target = tunnel_path_match.groups()[0]
+
+            return Tunnel(client_host, target)
+
+        return RegularView()
+
+
+class Tunnel(Resource):
     isLeaf = True
-    def render_GET(self, request):
-        return "<html><body>%s</body></html>" % (time.ctime(),)
 
-resource = ClockPage()
-factory = Site(resource)
+    def __init__(self, client_host, target):
+        Resource.__init__(self)
+        self.target = target
+        self.client_host = client_host
+
+        p = urlsplit(target)
+
+        self.target_base = urlunsplit((p.scheme, p.netloc, '', '', ''))
+
+    def render_POST(self, request):
+        return self.render_request(request, requests.post)
+
+    def render_GET(self, request):
+        return self.render_request(request, requests.get)
+
+    def render_request(self, twisted_request, requests_method):
+        global cookies
+
+        r = requests_method(self.target, cookies=cookies, data=twisted_request.args)
+
+        for k,v in r.headers.items():
+            if k in ['Content-Type', 'Content-Length']:
+                twisted_request.setHeader(k, v)
+
+        # make sure to store cookies if we get them, to reuse when actually returning
+        # the page to the browser, usually login state and session id is stored there
+        if dict(r.cookies):
+            cookies = dict(r.cookies)
+
+        # replace links only in html content
+        if not re.search("html", r.headers['Content-Type']):
+            return r.content
+
+        content = replace_links(r.text, self.client_host, self.target_base)
+
+        return content.encode('utf-8')
+
+
+class RegularView(Resource):
+    isLeaf = True
+
+    def render_GET(self, request):
+        return "<html><body><pre>%s</pre></body></html>" % request.path
+
+
+
+root = Dispatch()
+factory = Site(root)
 reactor.listenTCP(8880, factory)
 reactor.run()
--- tunneler.py
+++ tunneler.py
@@ -4,14 +4,14 @@ import re
 
 # point any relaitive links to be served via tunnel
 # i.e. '(stuff)="/<link>"' to '(stuff)="<client_tunnel_url>/link"'
-def _replace_relative(content, client_base, target):
-    tunneled = "\g<1>=\g<2>%s/%s/\g<3>\g<4>" % (client_base, target)
+def _replace_relative(content, client_host, target_base):
+    tunneled = "\g<1>=\g<2>%s/%s/\g<3>\g<4>" % (client_host, target_base)
     return re.sub("(data-source|data-url|src|action|href)=('|\")/(\S*)('|\")", tunneled, content)
 
 # absolute links are easier, as we can just add a tunnel url in front of absolute remote one
-def _replace_absolute(content, client_base, target):
-    return content.replace(target, "%s/%s" % (client_base, target))
+def _replace_absolute(content, client_host, target_base):
+    return content.replace(target_base, "%s/%s" % (client_host, target_base))
 
-def replace_links(content, client_base, target):
-    absolute = _replace_absolute(content, client_base, target)
-    return _replace_relative(absolute, client_base, target)
+def replace_links(content, client_host, target_base):
+    absolute = _replace_absolute(content, client_host, target_base)
+    return _replace_relative(absolute, client_host, target_base)
